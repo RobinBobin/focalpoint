b71db6415dd822ab5f9e78affa68e23ab264a1df
Ruben Shalimov <r_shalimov@inbox.ru>
2022-05-08 00:40:09 +0300

    `react-native-anchor-point` added.

--- App.tsx
+++ App.tsx
@@ -1,13 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, TransformsStyle, View } from 'react-native'
+import { withAnchorPoint } from 'react-native-anchor-point'
+
+const height = 262.4
+const width = 393.6
+
+const focalPoint = {
+  x: width / 2,
+  y: height
+}
+
+const translateY = height / 2 - focalPoint.y
 
 export default function App() {
+  let transform: TransformsStyle = {
+    transform: [
+      { scale: 1.5 },
+      { translateY: 50 }
+    ]
+  }
+
+  transform = withAnchorPoint(
+    transform,
+    {
+      x: 1,
+      y: 1
+    },
+    {
+      height,
+      width
+    }
+  )
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View
+        style={{
+          borderColor: 'blue',
+          borderStyle: 'dotted',
+          borderWidth: 2.5,
+        }}
+      >
+        <View
+          style={{
+            overflow: 'hidden'
+          }}
+        >
+          <Image
+            source={require('./assets/image.jpg')}
+            style={[
+              {
+                height,
+                width
+              },
+              transform
+            ]}
+          />
+        </View>
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +68,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
